af572f130dd3f66f17a70f654823b5b5c5a4bec2
Dawid24 <paciorekdawidonly@gmail.com>
2019-03-05 19:03:41 +0100

    add a category entity

--- src/main/java/pl/dawid/model/Category.java
+++ src/main/java/pl/dawid/model/Category.java
@@ -1,15 +1,72 @@
 package pl.dawid.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.List;
 
 @Entity
 public class Category implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    private String name;
+    @Column(length = 512)
+    private String description;
+
+    @OneToMany
+    @JoinColumn(name = "category_id", referencedColumnName = "id")
+    private List<Device> devices;
+
+    public Category() {
+
+    }
+    public Category(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Device> getDevices() {
+        return devices;
+    }
+
+    public void setDevices(List<Device> devices) {
+        this.devices = devices;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", devices=" + devices +
+                '}';
+    }
 }
