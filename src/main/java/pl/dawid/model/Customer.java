531009289bc77c5962b51307edb25e9d38a4229f
Dawid24 <paciorekdawidonly@gmail.com>
2019-03-05 19:40:19 +0100

    add a customer entity

---
+++ src/main/java/pl/dawid/model/Customer.java
@@ -0,0 +1,59 @@
+package pl.dawid.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class Customer implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    private String name;
+    private String surname;
+    private String pesel;
+
+    public Customer() {
+
+    }
+
+    public Customer(String name, String surname, String pesel) {
+        this.name = name;
+        this.surname = surname;
+        this.pesel = pesel;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    public void setPesel(String pesel) {
+        this.pesel = pesel;
+    }
+}
