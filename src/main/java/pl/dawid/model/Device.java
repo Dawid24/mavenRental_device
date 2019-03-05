6a7692bc75dd3bd84f164fcca05cd96efb5ba623
Dawid24 <paciorekdawidonly@gmail.com>
2019-03-05 15:44:12 +0100

    add a dependencies 1.1

--- src/main/java/pl/dawid/model/Device.java
+++ src/main/java/pl/dawid/model/Device.java
@@ -1,4 +1,79 @@
 package pl.dawid.model;
 
-public class Device {
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class Device implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private Integer quantity;
+    private Double price;
+
+    public Device() {
+
+    }
+
+    public Device(String name, String description, Integer quantity, Double price) {
+        this.name = name;
+        this.description = description;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Device{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", quantity=" + quantity +
+                ", price=" + price +
+                '}';
+    }
 }
